3545d0113826e9d05fa7f39540b45501f426b785
Marvellous <itzmhizterlouis@gmail.com>
2024-01-21 17:25:13 +0100

    Connected application to Postgresql Database

--- TimetableGenerator/settings.py
+++ TimetableGenerator/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-!@&t0)b8)p_s0%oi#aw7-1x#)92%7egzxa^u*8nc^+-&!3umut"
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -78,10 +78,14 @@ WSGI_APPLICATION = "TimetableGenerator.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': config('DATABASE_NAME'),
+       'USER': config('DATABASE_USERNAME'),
+       'PASSWORD': config('DATABASE_PASSWORD'),
+       'HOST': config('DATABASE_HOST_NAME'),
+       'PORT': config('DATABASE_PORT'),
+   }
 }
 
 
